8dc7635ac46726ab88e44a3299915ec1c762527f
Long Phung <43488406+longphungtuan94@users.noreply.github.com>
2018-11-07 15:08:04 +0700

    Add files via upload

--- segmentation.py
+++ segmentation.py
@@ -4,6 +4,10 @@ function characterRatioCheck """
 import cv2
 import numpy as np
 
+import imutils
+from skimage.filters import threshold_local
+from skimage import measure
+
 # Check if a contour has the features of a digit
 def isCharacter(plate_image, contour):
     (_, _, contour_width, contour_height) = cv2.boundingRect(contour)
@@ -73,5 +77,74 @@ def segment_from_plate(plate_image):
         # cv2.waitKey(0)
         # cv2.destroyAllWindows()
         return characterImageList
+    else:
+        return None
+
+def segment_characters_from_plate(plate_img, fixed_width):
+    # extract the Value component from the HSV color space and apply adaptive thresholding
+    # to reveal the characters on the license plate
+    V = cv2.split(cv2.cvtColor(plate_img, cv2.COLOR_BGR2HSV))[2]
+    T = threshold_local(V, 29, offset=15, method='gaussian')
+    thresh = (V > T).astype('uint8') * 255
+    thresh = cv2.bitwise_not(thresh)
+
+    # resize the license plate region to a canoncial size
+    plate_img = imutils.resize(plate_img, width=fixed_width)
+    thresh = imutils.resize(thresh, width=fixed_width)
+    
+    # perform a connected components analysis and initialize the mask to store the locations
+    # of the character candidates
+    labels = measure.label(thresh, neighbors=8, background=0)
+    charCandidates = np.zeros(thresh.shape, dtype='uint8')
+
+    # loof over the unique components
+    for label in np.unique(labels):
+        # if this is the background label, ignore it
+        if label == 0:
+            continue
+        # otherwise, construct the label mask to display only connected components for the
+        # current label, then find contours in the label mask
+        labelMask = np.zeros(thresh.shape, dtype='uint8')
+        labelMask[labels == label] = 255
+        cnts = cv2.findContours(labelMask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+        cnts = cnts[0] if imutils.is_cv2() else cnts[1]
+        
+        # ensure at least one contour was found in the mask
+        if len(cnts) > 0:
+            # grab the largest contour which corresponds to the component in the mask, then
+            # grab the bounding box for the contour
+            c = max(cnts, key=cv2.contourArea)
+            (boxX, boxY, boxW, boxH) = cv2.boundingRect(c)
+            
+            # compute the aspect ratio, solodity, and height ration for the component
+            aspectRatio = boxW / float(boxH)
+            solidity = cv2.contourArea(c) / float(boxW * boxH)
+            heightRatio = boxH / float(plate_img.shape[0])
+            
+            # determine if the aspect ratio, solidity, and height of the contour pass
+            # the rules tests
+            keepAspectRatio = aspectRatio < 1.0
+            keepSolidity = solidity > 0.15
+            keepHeight = heightRatio > 0.5 and heightRatio < 0.95
+            
+            # check to see if the component passes all the tests
+            if keepAspectRatio and keepSolidity and keepHeight and boxW > 10:
+                # compute the convex hull of the contour and draw it on the character
+                # candidates mask
+                hull = cv2.convexHull(c)
+                cv2.drawContours(charCandidates, [hull], -1, 255, -1)
+    # cv2.imshow('test', charCandidates)
+    # cv2.waitKey(0)
+    _, contours, hier = cv2.findContours(charCandidates, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    if contours:
+        contours = sort_contours_left_to_right(contours)
+        characters = []
+        for c in contours:
+            (x,y,w,h) = cv2.boundingRect(c)
+            temp = plate_img[y:y+h, x:x+w]
+            # cv2.imshow('temp', temp)
+            # cv2.waitKey(0)
+            characters.append(temp)
+        return characters
     else:
         return None
